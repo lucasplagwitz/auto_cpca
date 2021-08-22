52283e768e4cc945cdab1074df3e15304818d765
Lucas Plagwitz <l.plagwitz@uni-muenster.de>
2021-08-22 21:54:30 +0200

    add docstrings

--- auto_cpca/auto_cpca.py
+++ auto_cpca/auto_cpca.py
@@ -14,34 +14,93 @@ class AutoCPCA(TransformerMixin, BaseEstimator):
     """
     Automatic Contrastive Principle Components (AutoCPCA).
 
-    preprocess_with_pca_dim: int
-            If this parameter is provided (and it is greater than n_features), then both the foreground and background
-            datasets undergo a preliminary round of PCA to reduce their dimension to this number. If it is not provided
-            but n_features > 1000, a preliminary round of PCA is automatically performed to reduce the
-            dimensionality to 1000.
-    """
+    Parameters
+    ----------
+    n_components: int or None, default=None
+        Number of components to keep.
+        If n_components is None all components are kept::
 
+            n_components == min(n_samples, n_features) - 1
+
+    alpha: float, dict or str, default=1
+        Background weighting parameter.
+
+        If alpha is a number, every background will be penalized with this factor.
+
+        If alpha is a dict, the backgrounds will be penalized individually.
+
+        If alpha is set to 'auto', AutoCPCA calculate a suitable alpha by run the
+        decomposition multiple times. Not implemented yet!
+
+    preprocess_with_pca_dim: int, default=1000
+        If this parameter is provided (and it is greater than n_features), then both the foreground and background
+        datasets undergo a preliminary round of PCA to reduce their dimension to this number. If it is not provided
+        but n_features > 1000, a preliminary round of PCA is automatically performed to reduce the
+        dimensionality to 1000.
+
+    norm_cov_mean: bool, default=False
+        Normalize mean of covariance.
+
+    verbose: int, default=1
+        Print reduce message for PCA-based preprocessing.
+
+    Attributes
+    ----------
+    components_ : ndarray of shape (n_components, n_features)
+        Principal axes in feature space, representing the directions of
+        maximum variance in the data with respect to fore- and background.
+
+    """
     def __init__(self, n_components: int = None, alpha: Union[int, float, dict, str] = 1,
                  preprocess_with_pca_dim: int = 1000, norm_cov_mean: bool = False, verbose: int = 1):
-
         self.n_components = n_components
         self.preprocess_with_pca_dim = preprocess_with_pca_dim
         self.verbose = verbose
         self.alpha = alpha
-        self.classes_ = None
-
         self.fg_cov = None
         self.bg_cov = {}
-        self.v_top = None
-        self.pca = None
         self.norm_cov_mean = norm_cov_mean
+        self.pca = None
+
+        self.classes_ = None
+        self.components_ = None
 
     def fit_transform(self, X: np.ndarray, y: np.ndarray):
+        """Fit the model with X and apply the dimensionality reduction on X.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data, where n_samples is the number of samples
+            and n_features is the number of features.
+        y : array-like of shape (n_samples,)
+            Target vector relative to X.
+
+        Returns
+        -------
+        X_new : ndarray of shape (n_samples, n_components)
+            Transformed values.
+
+        """
         self.fit(X, y)
         return self.transform(X, y)
 
     def fit(self, X: np.ndarray, y: np.ndarray):
+        """Fit the model with X.
 
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data, where n_samples is the number of samples
+            and n_features is the number of features.
+        y : array-like of shape (n_samples,)
+            Target vector relative to X.
+
+        Returns
+        -------
+        self : object
+            Returns the instance itself.
+        """
         check_classification_targets(y)
         self.classes_ = np.unique(y)
 
@@ -51,7 +110,7 @@ class AutoCPCA(TransformerMixin, BaseEstimator):
         if self.n_components is None:
             self.n_components = min(np.min(X.shape), self.preprocess_with_pca_dim) - 1
 
-        fg, bgs = self.divide(X, y)  # backgrounds: list of n_classes
+        fg, bgs = self._divide(X, y)  # backgrounds: dict of n_classes
 
         if fg.shape[1] > self.preprocess_with_pca_dim:
             max_dimensions = np.min([len(fg), self.preprocess_with_pca_dim])
@@ -77,46 +136,95 @@ class AutoCPCA(TransformerMixin, BaseEstimator):
 
         if self.alpha == "auto":
             msg = "Not yet."
-            alpha = self.optimal_alpha(X, y)
+            alpha = self._optimal_alpha(X, y)
         elif isinstance(self.alpha, dict):
             alpha = self.alpha
         elif isinstance(self.alpha, (int, float)):
             alpha = {label: self.alpha for label in self.classes_}
-        self.create_component(alpha=alpha)
+        self._create_component(alpha=alpha)
 
         return self
 
     def transform(self, X, y=None):
+        """Apply dimensionality reduction to X.
 
+        X is projected on the first principal components previously extracted
+        from a training set.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            New data, where n_samples is the number of samples
+            and n_features is the number of features.
+        y: ignored
+
+        Returns
+        -------
+        reduced_dataset : array-like, shape (n_samples, n_components)
+        """
         check_is_fitted(self)
 
         # transform - feature selection via PCA for many features
         if self.pca is not None and X.shape[1] > self.preprocess_with_pca_dim:
             X = self.pca.transform(X)
-        reduced_dataset = X.dot(self.v_top)
+        reduced_dataset = X.dot(self.components_)
         return reduced_dataset
 
+    def feature_influence(self, weight=1):
+        """Sums influence of features to principle components.
+
+        Thought for feature importance.
+
+        Parameters
+        ----------
+        weight: float or array with shape (n_components, )
+            Multiply component by weight.
+
+        Returns
+        -------
+            Weighted sum of positive feature-influence in principle component.
+        """
+        check_is_fitted(self)
+        return np.sum(weight*np.abs(self.components_), axis=1)
+
     def inverse_transform(self, X, y=None):
-        X = X.dot(self.v_top.T)
+        """Transform data back to its original space.
+        In other words, return an input X_original whose transform would be X.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_components)
+            New data, where n_samples is the number of samples
+            and n_components is the number of components.
+        y: ignored
+
+        Returns
+        -------
+        X_original array-like, shape (n_samples, n_features)
+        """
+        check_is_fitted(self)
+        X_original = X.dot(self.components_.T)
         if self.pca is not None:
-            X = self.pca.inverse_transform(X)
-        return X
+            X_original = self.pca.inverse_transform(X_original)
+        return X_original
 
-    def create_component(self, alpha: np.ndarray):
-
+    def _create_component(self, alpha: np.ndarray):
+        """Apply SVD on sigma."""
         sigma = self.fg_cov - sum([alpha[key]*val for key, val in self.bg_cov.items()])
         w, v = np.linalg.eigh(sigma)
         eig_idx = np.argpartition(w, -self.n_components)[-self.n_components:]
         eig_idx = eig_idx[np.argsort(-w[eig_idx])]
-        self.v_top = np.real(v[:, eig_idx])  # sigma is quasi symm. -> imaginary part only numerical reasons
+        self.components_ = np.real(v[:, eig_idx])  # sigma is quasi symm. -> imaginary part only numerical reasons
 
-    def divide(self, X, y):
+    def _divide(self, X, y):
+        """Calculate foreground and backgrounds."""
         bgs = {}
         for label in self.classes_:
             bgs[label] = X[y == label]
         return X, bgs
 
-    def optimal_alpha(self, X, y, metric=balanced_accuracy_score, start_value=1, n_iter=500):
+    def _optimal_alpha(self, X, y, metric=balanced_accuracy_score, start_value=1, n_iter=500):
+        """Optimal parameter-selection procedure."""
         msg = "Not ready yet - please use hyperparameter optimization for the alpha-choice. " \
               "A suitable selection-algorithm will follow in the future. "
         raise NotImplemented(msg)
@@ -150,7 +258,3 @@ class AutoCPCA(TransformerMixin, BaseEstimator):
         msg = "Not converged"
         warnings.warn(msg)
         return alpha
-
-    def feature_influence(self, weight=1):
-        check_is_fitted(self)
-        return np.sum(weight*np.abs(self.v_top), axis=1)
--- examples/mnist_2d_plot.py
+++ examples/mnist_2d_plot.py
@@ -1,25 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from auto_cpca import AutoCPCA
 from sklearn.decomposition import PCA
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.datasets import fetch_openml
 
+from auto_cpca import AutoCPCA
+
 # some data preprocessing
 X, y = fetch_openml('mnist_784', version=1, return_X_y=True, as_frame=False)
 X, y = X[:5000], y[:5000].astype(int)
-# CPCA with background
-y_binary = (y == 2).astype(int)*2 + (y == 7).astype(int)*7
+# AutoCPCA with background
+y_selection = (y == 2).astype(int)*2 + (y == 7).astype(int)*7
 
 # comparison between both
 auto_cpca = AutoCPCA(n_components=2, alpha={0: 7, 2: 0, 7: 0})
 normal_pca = PCA(n_components=2)
 
-# build fg and bg only on train_set
+# build fg and bgs only on train_set
 a = StratifiedShuffleSplit(1, test_size=0.1)
 train_index, test_index = next(a.split(X, y))
-auto_cpca.fit(X[train_index], y_binary[train_index])
-normal_pca.fit(X[train_index], y_binary[train_index])
+auto_cpca.fit(X[train_index], y_selection[train_index])
+normal_pca.fit(X[train_index], y_selection[train_index])
 
 trans_normal_pca_X = normal_pca.transform(X[test_index])
 trans_X = auto_cpca.transform(X[test_index])
@@ -29,23 +30,18 @@ plt.subplot(2, 2, 1)
 plt.scatter(trans_normal_pca_X[:, 0], trans_normal_pca_X[:, 1], c=y[test_index],
             cmap=plt.cm.coolwarm, alpha=0.6)
 plt.title('PCA all classes')
-
 plt.subplot(2, 2, 2)
-plt.scatter(trans_normal_pca_X[:, 0], trans_normal_pca_X[:, 1], c=y_binary[test_index],
+plt.scatter(trans_normal_pca_X[:, 0], trans_normal_pca_X[:, 1], c=y_selection[test_index],
             cmap=plt.cm.coolwarm, alpha=0.6)
-plt.title('PCA binary classes')
-
+plt.title('PCA selected classes')
 plt.subplot(2, 2, 3)
 plt.scatter(trans_X[:, 0], trans_X[:, 1], c=y[test_index],
             cmap=plt.cm.coolwarm, alpha=0.6)
 plt.title('AutoCPCA all classes')
-
 plt.subplot(2, 2, 4)
-plt.scatter(trans_X[:, 0], trans_X[:, 1], c=y_binary[test_index],
+plt.scatter(trans_X[:, 0], trans_X[:, 1], c=y_selection[test_index],
             cmap=plt.cm.coolwarm, alpha=0.6)
-plt.title('AutoCPCA binary classes')
-
-
+plt.title('AutoCPCA selected classes')
 plt.tight_layout()
 plt.show()
 plt.close()
